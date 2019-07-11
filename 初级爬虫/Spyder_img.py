042a298dd03595444577003a18089ae5079b2291
汪瑞 <1258331093@qq.com>
2019-07-11 19:32:57 +0800

    添加注释

--- 初级爬虫/Spyder_img.py
+++ 初级爬虫/Spyder_img.py
@@ -6,16 +6,20 @@ import jsonpath
 # 获取网页源码-->解析-->筛选-->保存
 
 def get_web_content(url):
+    #获取网页源代码
     response = requests.get(url)
     html = response.text
     return html
 
 def analyse_web(html):
+    #将数据转为json格式
     data = json.loads(html)
+    #得到图片的url
     photos = jsonpath.jsonpath(data, '$..path')
     return photos
 
 def save(photos):
+    #保存图片
     global num
     for i in photos:
         photo = requests.get(i)
@@ -25,14 +29,17 @@ def save(photos):
             
 def main(label):
     num = 0
+    #关键字加密，对应url中的kw={}
     label = urllib.parse.quote(label)
     url = 'https://www.duitang.com/napi/blog/list/by_search/?kw={}&start={}'
+    #以24为间隔是通过分析网页源码得到的
     for index in range(1, 241, 24):
+        #传入关键字和开始值，index对应的是url中的start={}
         urls = url.format(label, index)
         html = get_web_content(urls)
         photos = analyse_web(html)
         save(photos)
         
 if __name__ == '__main__':
-#关键字可以改
+    #关键字可以改成别的，如'数'，'人'等
     main('天空')
